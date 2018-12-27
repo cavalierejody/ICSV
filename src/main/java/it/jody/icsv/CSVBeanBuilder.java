5a03c31ad21ec7332eade69a94c15b101d80569f
cavalierejody <cavalierejody@yahoo.it>
2018-12-27 02:56:24 +0100

    updated builder

--- src/main/java/it/jody/icsv/CSVBeanBuilder.java
+++ src/main/java/it/jody/icsv/CSVBeanBuilder.java
@@ -7,49 +7,39 @@ import java.util.HashMap;
 /**
  * Created by Jody on 20/02/2018.
  */
-public class CSVBeanBuilder<T> {
+public class CSVBeanBuilder {
 
     private HashMap<String, Class> managedTypes = new HashMap<String, Class>();
 
-    public T build(Class type) {
-
-        CSVType annotationMarker = (CSVType) type.getAnnotation(CSVType.class);
-        Object[] array = new Object[annotationMarker.size()];
-        if (! annotationMarker.markerName().isEmpty()) {
-            managedTypes.put(annotationMarker.markerName(), type);
-            array[0] = annotationMarker.markerName();
-        }
-        return (T) Proxy.newProxyInstance(
-                CSVBeanBuilder.class.getClassLoader(),
-                new Class[] { type },
-                new CSVInvocationHandler(array)
-        );
-    }
-
-    // try wih
+    /**
+     * Return bean instance for an array.
+     * It can be used both with managed types and unmaged types of aarray.
+     * If array type is managed, then the managed types library will be updated.
+     * @param array
+     * @return
+     */
     public Object build(Class type, Object[] array) {
 
-        CSVType annotationMarker = (CSVType) type.getAnnotation(CSVType.class);
-        if (annotationMarker != null) {
-            String markerName = annotationMarker.markerName();
-            if (markerName != null && !markerName.isEmpty()) {
-                if (! markerName.equals(array[0])) {
-                    throw new RuntimeException("invalid markerName for array");
-                }
-            }
-        }
-        //TODO refactor in factory for builder
-        return (T) Proxy.newProxyInstance(
+        withManagedType(type);
+
+        return Proxy.newProxyInstance(
                 CSVBeanBuilder.class.getClassLoader(),
                 new Class[] { type },
                 new CSVInvocationHandler(array)
         );
     }
 
+    /**
+     * Return bean instance for an array with a managed type already loaded.
+     * It cannot be used with unmanaged tyupes of array.
+     * @param array
+     * @return
+     */
     public Object build(Object[] array) {
 
         String typeMarker = String.valueOf(array[0]);
         Class aClass = managedTypes.get(typeMarker);
+
         if (aClass == null) {
             throw new RuntimeException("unmanaged type of array");
         } else {
@@ -57,19 +47,32 @@ public class CSVBeanBuilder<T> {
         }
     }
 
+    /**
+     * Register a type Class for Bean conversion based on the marker attribute.
+     * @param type
+     * @return
+     */
     public CSVBeanBuilder withManagedType(Class type) {
 
-        CSVType CSVType = (CSVType) type.getAnnotation(CSVType.class);
-        if (CSVType == null)
-            throw new RuntimeException("missing CSVType annotation for type " + type.getCanonicalName());
-        if (CSVType.markerName().isEmpty()) {
-            throw new RuntimeException("missing markerName annotation");
+        CSVType csvType = (CSVType) type.getAnnotation(CSVType.class);
+        if (csvType != null) {
+            if (csvType.markerName().isEmpty()) {
+                throw new RuntimeException("missing markerName annotation");
+            } else {
+                String key = csvType.markerName();
+                managedTypes.put(key, type);
+            }
         }
-        String key = CSVType.markerName();
-        managedTypes.put(key, type);
+
         return this;
     }
 
+    /**
+     * Register multiple types for Bean conversion based on the marker attribute.
+     * It calls method {@link #withManagedType(java.lang.Class)}
+     * @param types
+     * @return
+     */
     public CSVBeanBuilder withManagedTypes(Collection<Class> types) {
 
         for (Class type : types) {
